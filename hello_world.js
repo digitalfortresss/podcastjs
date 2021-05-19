a4203c43a7f374d2a32bdeae08c9d5ce078a9a83
dzeninside <dzeninside@gmail.com>
2021-05-19 04:32:19 +0300

    first commit

---
+++ hello_world.js
@@ -0,0 +1,31 @@
+let showIndexHtml = () => {
+
+    let lalala = `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>`
+    let a = document.getElementById('wrap_html_h').textContent = lalala;
+
+
+    
+    
+    
+    
+
+
+    console.log(a)
+
+}
+// showIndexHtml()
+
+
+
+3 ? console.log(1) : console.log(2)
